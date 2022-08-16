badf53df2fdc2cff6b995be8670347578b9a2751
zuhayrtariq <111240413+zuhayrtariq@users.noreply.github.com>
2022-08-17 01:55:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function checkCustomRadio() {
+    const customRadio = document.querySelector('#customAmountRadio');
+    const customAmountInput = document.querySelector('#customAmountInput');
+    if (customRadio.checked) {
+
+        customAmountInput.required = true;
+    } else {
+
+        customAmountInput.required = false;
+    }
+};
+
+const stripe = Stripe("STRIPE PUBLISHABLE KEY")
